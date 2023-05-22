939ef17c2acae895ae6c84187df55b89c6814c73
Pranit Sahebrao Sahane <90859405+pspranit@users.noreply.github.com>
2023-05-22 11:09:34 +0530

    Add files via upload

---
+++ STUDENTCRUD_REST_API/src/test/java/com/jspiders/STUDENTCRUD_REST_API/StudentcrudRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jspiders.STUDENTCRUD_REST_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentcrudRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
