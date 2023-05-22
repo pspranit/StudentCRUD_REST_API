f772e0d22acce6d833d5c386c3e87b11607bea3d
Pranit Sahebrao Sahane <90859405+pspranit@users.noreply.github.com>
2023-05-22 11:05:11 +0530

    Delete STUDENTCRUD_REST_API directory

--- STUDENTCRUD_REST_API/src/main/java/com/jspiders/STUDENTCRUD_REST_API/Repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jspiders.STUDENTCRUD_REST_API.Repository;
-
-import com.jspiders.STUDENTCRUD_REST_API.Model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-
-}
